b6a47e78aa589577fb7110ee152c7b680995bce7
Sujit_0210 <155601038+SujitYalmar@users.noreply.github.com>
2025-01-07 16:32:16 +0530

    Time: 50 ms (5.01%), Space: 45.2 MB (5.38%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isValid(String s) {
+        while(true){
+            if(s.contains("()")){
+                s=s.replace("()", "");
+            }
+            else if(s.contains("{}")){
+                s = s.replace("{}","");
+            }
+            else if(s.contains("[]")){
+                s=s.replace("[]", "");
+            }
+            else{
+                return s.isEmpty();
+            }
+        }
+        
+    }
+}
